fb2410d4977b9c5f8398bfd6a2a9b79d3d2e39de
Irving Zamora <irvingzamora28@gmail.com>
2024-10-16 11:15:13 -0600

    Correct output dist directory

--- build-all.sh
+++ build-all.sh
@@ -2,5 +2,5 @@
 mkdir -p dist
 for episode in season1/*; do
   episode_name=$(basename "$episode")
-  slidev build "$episode/slides.md" --out "$(pwd)/dist/$episode_name" --base "/season1/$episode_name/"
+  slidev build "$episode/slides.md" --out "$(pwd)/dist/season1/$episode_name" --base "/season1/$episode_name/"
 done
