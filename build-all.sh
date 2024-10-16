625cc17c91b6e77ce06847acc2976730eccdaffc
Irving Zamora <irvingzamora28@gmail.com>
2024-10-16 11:07:05 -0600

    Fix output path to ensure builds are placed in the correct dist directory

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+mkdir -p dist
 for episode in season1/*; do
   episode_name=$(basename "$episode")
-  slidev build "$episode/slides.md" --out "dist/$episode_name" --base "/season1/$episode_name/"
+  slidev build "$episode/slides.md" --out "$(pwd)/dist/$episode_name" --base "/season1/$episode_name/"
 done
